783c03c4ed6f6fcd46fed612f70571775f189e0d
nkadze <mezurnishvili.giorgi@gmail.com>
2015-10-28 22:06:04 +0400

    mapael raphael jotto\

---
+++ js/maps_init.js
@@ -0,0 +1,15 @@
+$(function(){
+	// $(".mapcontainer").mapael({
+	// 	map : {
+ //            // Set the name of the map to display
+	// 		name : "france_departments"
+	// 	}
+	// });
+
+	$(".mapcontainer").mapael({
+		map : {
+            // Set the name of the map to display
+			name : "china_map"
+		}
+	});
+});
