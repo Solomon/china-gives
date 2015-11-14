2d6055fdfac380a287e9af56f5563c37d29c46af
nkadze <mezurnishvili.giorgi@gmail.com>
2015-11-14 20:14:22 +0400

    translations started

---
+++ js/translate.js
@@ -0,0 +1,82 @@
+// var words_in_list;
+
+
+// String.prototype.trsl = function () {
+
+// 	if (!is_chinese())
+// 		return this;
+
+// console.log('!');
+	
+// 	if (!words_in_list)
+// 		words_in_list = word_list();
+
+// 	var word = jQuery.grep(words_in_list, function(item) {
+//                         return item.en == this;
+//                       });
+// 	if (word.length > 0)
+// 	{
+// 		return word[0].cn;
+// 	}
+//     return this;
+// }
+
+// Array.prototype.trsl_arr = function () {
+// 	var arr = [];
+// 	for (var i = 0; i < this.length; i++) {
+// 		arr.push(this[i].trsl());
+// 	};
+//     return arr;
+// }
+
+// function is_chinese(){
+// 	return window.location.href.indexOf('/cn/') >= 0;
+// }
+
+
+// function word_list(){
+// 	var words = [
+// 		{
+// 			en: 'Manufacturing',
+// 			cn: '制造业'
+// 		},
+// 		{
+// 			en: 'Real Estate',
+// 			cn: '房地产'
+// 		},
+// 		{
+// 			en: 'Energy',
+// 			cn: '能源'
+// 		},
+// 		{
+// 			en: 'Tech/IT',
+// 			cn: '科技'
+// 		},
+// 		{
+// 			en: 'Finance',
+// 			cn: '金融'
+// 		},
+// 		{
+// 			en: 'Education',
+// 			cn: '教育'
+// 		},
+// 		{
+// 			en: 'Consumer',
+// 			cn: '消费'
+// 		},
+// 		{
+// 			en: 'Healthcare',
+// 			cn: '医疗'
+// 		},
+// 		{
+// 			en: 'Transportation',
+// 			cn: '交通'
+// 		},
+// 		{
+// 			en: 'Other',
+// 			cn: '其他'
+// 		}
+// 	];
+
+// 	return words;
+// }
